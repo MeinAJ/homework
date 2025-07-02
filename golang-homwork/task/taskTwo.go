f8a4dca6d29bafe51d62fc2e8738d859274777ae
aj <15102356906@163.com>
2025-07-02 23:33:48 +0800

    commit

--- golang-homwork/task/taskTwo.go
+++ golang-homwork/task/taskTwo.go
@@ -43,7 +43,7 @@ func PrintOddAndEvenFrom0To10(group *sync.WaitGroup) {
 	}()
 }
 
-// 设计一个任务调度器，接收一组任务（可以用函数表示），并使用协程并发执行这些任务，同时统计每个任务的执行时间。
+// PrintAsync 设计一个任务调度器，接收一组任务（可以用函数表示），并使用协程并发执行这些任务，同时统计每个任务的执行时间。
 func PrintAsync(index int) {
 	now := time.Now()
 	// 创建新的随机数生成器
