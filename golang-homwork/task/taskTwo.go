daed5df1693fbe03487835121d2fc3670ff8fe42
MeinAJ <15102356906@163.com>
2025-07-07 17:13:54 +0800

    Update taskTwo.go

--- golang-homwork/task/taskTwo.go
+++ golang-homwork/task/taskTwo.go
@@ -192,3 +192,19 @@ func SyncMutexCount() {
 	fmt.Print("count:", count)
 }
 // 使用原子操作（ sync/atomic 包）实现一个无锁的计数器。启动10个协程，每个协程对计数器进行1000次递增操作，最后输出计数器的值。
+func AtomicCounter() {
+	atomicCount := atomic.NewInt32(0)
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(10)
+	for i := 0; i < 10; i++ {
+		go func(atomicCount *atomic.Int32) {
+			defer waitGroup.Done()
+			for i := 0; i < 1000; i++ {
+				time.Sleep(10 * time.Millisecond)
+				atomicCount.Inc()
+			}
+		}(atomicCount)
+	}
+	waitGroup.Wait()
+	fmt.Println("atomicCount：", atomicCount.Load())
+}
