73b15ce24d24da8d3a8b7b0e7f19869347f62e2b
aj <15102356906@163.com>
2025-07-17 22:53:28 +0800

    commit

--- solidity/task/taskThree/hardhat.config.js
+++ solidity/task/taskThree/hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+require('hardhat-deploy');
+require("@openzeppelin/hardhat-upgrades")
 
 module.exports = {
   solidity: "0.8.28",
@@ -7,6 +9,11 @@ module.exports = {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
       accounts: [process.env.WALLET_PRIVATE_KEY] // 使用钱包私钥
+    },
+    namedAccounts: {
+      deployer: 0,
+      user1: 1,
+      user2: 2,
     }
   }
 };
